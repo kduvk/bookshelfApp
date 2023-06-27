8cf10b7be038abe5f9551b6f44d1d3698ae11402
kduvk <harshjagda@proton.me>
2023-06-27 17:07:59 +0400

    basic login page UI implemented

--- bookshelf_app/lib/screens/login/login.dart
+++ bookshelf_app/lib/screens/login/login.dart
@@ -6,26 +6,64 @@ class OLogin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              "Log In.",
-              softWrap: true,
-              style: TextStyle(color: Colors.white),
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            'assets/login.jpg',
+            fit: BoxFit.cover,
+          ),
+          Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  "Log In",
+                  softWrap: true,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30.0,
+                  ),
+                ),
+                const SizedBox(height: 10.0),
+                _textField("Email"),
+                const SizedBox(height: 10.0),
+                _textField("Password"),
+                const SizedBox(height: 10.0),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // login logic
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                    ),
+                    child: const Text(
+                      "Login",
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 5.0),
+                TextButton(
+                  onPressed: () {
+                    // signup logic
+                  },
+                  child: const Text(
+                    "Don't have an account? Sign up",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(
-              height: 16.0,
-            ),
-            _textField("Email"),
-            const SizedBox(height: 16.0),
-            _textField("Password"),
-            const SizedBox(
-              height: 24.0,
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -33,19 +71,28 @@ class OLogin extends StatelessWidget {
   Widget _textField(String label) {
     return Container(
       decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10.0),
-          boxShadow: [
-            BoxShadow(
-                color: Colors.black.withOpacity(0.5),
-                offset: const Offset(0.0, 2.0),
-                blurRadius: 4.0)
-          ]),
-      child: TextField(
-        decoration: InputDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.5),
+            offset: const Offset(2.0, 2.0),
+            blurRadius: 4.0,
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+        child: TextField(
+          style: const TextStyle(color: Colors.black),
+          decoration: InputDecoration(
             labelText: label,
-            border:
-                OutlineInputBorder(borderRadius: BorderRadius.circular(10.0))),
+            labelStyle: const TextStyle(color: Colors.black54),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+          ),
+        ),
       ),
     );
   }
