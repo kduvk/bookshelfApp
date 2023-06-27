8ed56b7780accba0b9442d266d52e3dcc8a1642f
kduvk <harshjagda@proton.me>
2023-06-27 14:02:17 +0400

    basic login page UI implemented

--- bookshelf_app/lib/main.dart
+++ bookshelf_app/lib/main.dart
@@ -4,9 +4,12 @@ import 'package:bookshelf_app/screens/login/login.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
       home: OLogin(),
     );
   }
--- bookshelf_app/lib/screens/login/login.dart
+++ bookshelf_app/lib/screens/login/login.dart
@@ -1,19 +1,52 @@
 import 'package:flutter/material.dart';
 
 class OLogin extends StatelessWidget {
+  const OLogin({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                decoration: InputDecoration(labelText: 'Email'),
-              )
-            ],
-          )),
+    return Material(
+      child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              "Log In.",
+              softWrap: true,
+              style: TextStyle(color: Colors.white),
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+            _textField("Email"),
+            const SizedBox(height: 16.0),
+            _textField("Password"),
+            const SizedBox(
+              height: 24.0,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _textField(String label) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.5),
+                offset: const Offset(0.0, 2.0),
+                blurRadius: 4.0)
+          ]),
+      child: TextField(
+        decoration: InputDecoration(
+            labelText: label,
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(10.0))),
+      ),
     );
   }
 }
